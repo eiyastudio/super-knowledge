4e863e075702710287b4cb712c6ef4f50bb84998
tn <admin@eiya.studio>
2026-01-09 20:10:15 +0900

    feat: Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                article: resolve(__dirname, 'article.html'),
+                editor: resolve(__dirname, 'editor.html'),
+            },
+        },
+    },
+});
